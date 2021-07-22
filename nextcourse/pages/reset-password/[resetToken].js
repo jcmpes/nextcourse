ad6bb07d8c3960ba1d9f9fa8ca54ab2ba3a0baf8
JC <jcmpes@gmail.com>
2021-07-22 11:15:02 +0200

    feat: forgot password funciona

---
+++ nextcourse/pages/reset-password/[resetToken].js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch } from 'react-redux';
+import { resetPasswordAction } from '../../src/store/actions/reset-password';
+import Layout from '../../src/components/layout/Layout';
+import ResetPasswordForm from '../../src/components/auth/ResetPasswordPage/ResetPasswordForm';
+import { useRouter } from 'next/router';
+
+const ResetPasswordPage = () => {
+  const dispatch = useDispatch();
+  const history = useRouter();
+  const location = useRouter();
+
+  const { t } = useTranslation(['global']);
+
+  const { resetToken } = useRouter().query;
+
+  const handleSubmit = (newPassword) => {
+    dispatch(resetPasswordAction(resetToken, newPassword, history, location));
+    // TODO UX:
+    // React-toastify -> 'Password updated'
+  };
+
+  return (
+    <Layout>
+      <h1>{t('reset password.reset password')}</h1>
+      <p>{t('reset password.please enter your new password')}</p>
+      <ResetPasswordForm onSubmit={handleSubmit} />
+    </Layout>
+  );
+};
+
+export default ResetPasswordPage;
